253bca163237551c2d5a726bd8a6e1706daff954
shamil <for.photos.only001@gmail.com>
2026-03-09 23:03:57 +0530

    initial ui complete

--- app/component/AddTodo.tsx
+++ app/component/AddTodo.tsx
@@ -2,7 +2,7 @@ export default function AddTodo(){
     return (
         <>
         <div className="todo__container">
-            <input className="w-[500px] h-[20px] " type="text" /><button>add</button>
+            <input className="w-125 h-10 border-b-blue-900 bg-fuchsia-900 rounded-lg p-3 text-lg font-bold" type="text" /> <button className="rounded-lg px-5 py-2.5 font-extrabold text-3xl bg-indigo-950"> ADD</button>
 
         </div>
         </>
