a934612d078b1f57eeafd8b0c7aab44de810af23
shamil <for.photos.only001@gmail.com>
2026-03-21 22:32:10 +0530

    loading fix

--- app/(main)/todos/page.tsx
+++ app/(main)/todos/page.tsx
@@ -1,7 +1,13 @@
 // import ListItem from "../component/ListItem";
 import TodoList from "../../component/TodoList";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function TodosPage() {
+export default async function TodosPage() {
+  await new Promise(resolve => setTimeout(resolve, 3000))
+  const session = await getServerSession(authOptions)
+  if (!session) redirect('/login')
   return (
     <main className="">
       <div className="todo__container w-full m-2">
